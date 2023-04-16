b3f6300ea1b6c3e0e1c9463ce20b9bb9ed14bba3
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-16 11:39:48 +0400

    Completed the final task of the module

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,12 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int exp = numberToPrint;
+        for (int i = 1; i < power; i++) {
+            numberToPrint*=exp;
+            continue;
+        }
+        System.out.println(numberToPrint);
     }
 
     public static void main(String[] args) {
